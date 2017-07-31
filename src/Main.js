a4364c8bd19104da4f96662ef33d48275e3a3eed
kevin hu <hu.kevin.w@gmail.com>
2017-07-31 16:26:05 -0400

    Fix spacing for Main component

--- src/Main.js
+++ src/Main.js
@@ -1,31 +1,26 @@
-import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React, {Component} from 'react';
+import {Switch, Route} from 'react-router-dom';
 import Landing from './Landing';
 import EventRoom from './EventRoom';
 
 class Main extends Component {
-  render() {
-  const { currentUser, restaurants } = this.props
-  const FourOhFour = () => <h1>404</h1>;
-  const LandingPage = (props) => {
-    return (
-      <Landing
-        currentUser={currentUser}
-        restaurants={restaurants}
-        />
-      );
-    }
+    render() {
+        const {currentUser, restaurants} = this.props
+        const FourOhFour = () => <h1>404</h1>;
+        const LandingPage = (props) => {
+            return (<Landing currentUser={currentUser} restaurants={restaurants}/>);
+        }
 
-    return (
-      <div>
-       <Switch>
-         <Route exact path='/' render={LandingPage}/>
-         <Route path='/eventroom' component={EventRoom}/>
-         <Route default component={FourOhFour} />
-       </Switch>
-     </div>
-    );
-  }
+        return (
+            <div>
+                <Switch>
+                    <Route exact path='/' render={LandingPage}/>
+                    <Route path='/eventroom' component={EventRoom}/>
+                    <Route default component={FourOhFour}/>
+                </Switch>
+            </div>
+        );
+    }
 }
 
 export default Main;
