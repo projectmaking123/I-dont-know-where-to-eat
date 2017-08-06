a5d905625cc3f16c663856e2bcd90772e59088ab
kevin hu <hu.kevin.w@gmail.com>
2017-08-06 14:24:53 -0400

    Add info for instruction to navbar

--- src/App.js
+++ src/App.js
@@ -18,8 +18,10 @@ class App extends Component {
         this.state = {
             currentUser: null,
             restaurants: null,
-            users: {}
+            users: {},
+            showInstructions: false
         };
+        this.toggleInstructions = this.toggleInstructions.bind(this)
     }
 
     componentDidMount() {
@@ -48,40 +50,40 @@ class App extends Component {
         })
     }
 
-
+    toggleInstructions() {
+        this.setState(prevState => ({
+            showInstructions: !prevState.showInstructions
+        }));
+    }
 
     render() {
         const {currentUser, users, restaurants} = this.state
-
-
         return (
             <BrowserRouter key={Math.random()}>
                 <div className="">
                     <nav className="navbar" >
 
-                        <button className="btn btn-primary nav_btn">
-                            <a style={{
-                                color: 'white'
-                            }} href="/">Home</a>
-                        </button>
-                        <button className="btn btn-primary nav_btn">
-                            <a style={{
-                                color: 'white'
-                            }} href="/eventroom" title="Events">events</a>
-                        </button>
+                        <button className="btn btn-primary nav_btn"><a href="/">Home</a></button>
+                        <button className="btn btn-primary nav_btn"><a href="/eventroom" title="Events">events</a></button>
                         {
                             currentUser
                             ? <button className="btn btn-primary nav_btn" id="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
                             : <button className="btn btn-primary nav_btn" id="sign-in" onClick={() => auth.signInWithPopup(googleAuthProvider)}>Sign In</button>
                         }
+                        <button className="info_btn nav_btn" onClick={this.toggleInstructions}>?</button>
+                        <div className={"info_instructions " + (this.state.showInstructions ? "show_instruction" : "hide_instruction")}>
+                            <h1>
+                                Instructions
+                            </h1>
+                            <ul>
+                                <li>Enter a restaurant or restaurants you would like to invite everyone to</li>
+                                <li>Everyone then votes</li>
+                                <li>The choice with the most votes wins</li>
+                            </ul>
+                        </div>
                     </nav>
 
-                    <div>
-                        <p>
-                            hellooooooooooo
-                        </p>
-                    </div>
-                    
+
                     <Main currentUser={currentUser} restaurants={restaurants} users={users}/>
 
                 </div>
